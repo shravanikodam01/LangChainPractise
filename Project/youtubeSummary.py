1881af78c9818c3d6afca079be61d224f41499d2
shravanikodam01 <shravanik26@gmail.com>
2026-03-19 11:29:46 -0500

    Tried with LCEL, used RunnableParallel, RunnablePassThrough

--- Project/youtubeSummary.py
+++ Project/youtubeSummary.py
@@ -1,9 +1,11 @@
+from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
 
 load_dotenv()
 
@@ -40,20 +42,40 @@ prompt_template = ChatPromptTemplate(
 )
 
 #Creating the chat model
-chat = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.5)
+# chat = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.5)
 
-question = "What is the video about?"
+# question = "What is the video about?"
 
-#Getting the relevant documents and generating the answer
-relDoc = retriever.invoke(question)
+# #Getting the relevant documents and generating the answer
+# relDoc = retriever.invoke(question)
 
-context = "\n".join([doc.page_content for doc in relDoc])
+# context = "\n".join([doc.page_content for doc in relDoc])
 
-finalPrompt = prompt_template.invoke({ "context": context, "question": question })
-answer = chat.invoke(finalPrompt)
+# finalPrompt = prompt_template.invoke({ "context": context, "question": question })
+# answer = chat.invoke(finalPrompt)
 
-print(answer.content)
-
-
+# print(answer.content)
 
 
+# Using chains to implement the same thing
+
+def formatDocs(docs):
+    return "\n".join([doc.page_content for doc in docs])
+
+parallelChain = RunnableParallel({
+    "context": retriever | RunnablePassthrough(formatDocs),
+    "question": RunnablePassthrough()
+})
+
+
+
+parser = StrOutputParser()
+
+model = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.5)
+
+mainChain = parallelChain | prompt_template | model | parser
+
+question = "What is the video about? Is it talking about Aliens"
+answer = mainChain.invoke(question)
+
+print(answer)
